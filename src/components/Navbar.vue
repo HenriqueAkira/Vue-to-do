7ecb82bf428f03e986e5a61a6d2fde0eede21b2b
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-08-30 10:55:15 -0300

    Starting navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="grey--text"></v-app-bar-nav-icon>
+
+        <v-toolbar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">Todo</span>
+            <span>Vue</span>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        
+        <v-btn text color="grey"> 
+            <span>Sign Out</span>
+            <v-icon>mdi-exit-to-app</v-icon>
+        </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app class="indigo" temporary v-model="drawer">  
+        <p>test</p>
+    </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer: false
+        }
+    },
+    methods:{
+    }
+
+}
+</script>
+
+<style>
+
+</style>
