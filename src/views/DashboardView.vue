f016b3c097d3bf9bf8ba4a8b649670bb421c767c
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-08-31 15:02:55 -0300

    sort by

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,7 +5,20 @@
     <h1 class="subtitle-1 grey--text" >Dashboard</h1>
 
     <v-container fluid class="my-9 pa-16">
-      <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
+
+      <v-row class="mb-9">
+        <v-btn small depressed class="mr-5 grey--text" @click="sortBy('title')">
+          <v-icon left small>mdi-folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+
+        <v-btn small depressed class="mr-5 grey--text" @click="sortBy('person')">
+          <v-icon left small>mdi-account</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
+      </v-row>
+      
+      <v-card flat v-for="project in projects" :key="project.title">
         <v-row :class="`pa-3 project ${project.status}`" align="center">
           <v-col cols="12" md="6">
               <div class="caption grey--text">Project title</div>
@@ -68,12 +81,27 @@
 
     data() {
       return{
+        ascending: false,
+
         projects: [
         { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Hode up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+      }
+    },
+
+    methods: {
+      sortBy(prop){
+        if(!this.ascending){
+          this.projects.sort((a,b)=> a[prop] < b[prop] ? -1 : 1)
+          this.ascending = true
+        }else{
+          this.projects.sort((a,b)=> a[prop] < b[prop] ? 1 : -1)
+          this.ascending = false
+        }
       }
     }
   }
