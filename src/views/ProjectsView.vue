3f0d1d6f18a09124ad29ca135f94785d2081d8de
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-09-01 10:45:40 -0300

    projects expansion panel

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -5,12 +5,21 @@
 
     <v-container fluid class="my-6">
       
-      <v-row>
-        <v-col cols="12">
-            as
-        </v-col>   
-      </v-row>
-
+      <v-expansion-panels multiple >
+        <v-expansion-panel v-for="project in myProjects" :key="project.title">
+          <v-expansion-panel-header>
+            {{project.title}}
+          </v-expansion-panel-header>
+          <v-card class="px-4 grey--text">
+            <v-expansion-panel-content>
+              <div class="font-weight-bold">due by {{project.due}}</div>
+              <div>{{project.content}}</div>
+            </v-expansion-panel-content>
+          </v-card>
+        </v-expansion-panel>
+        
+      </v-expansion-panels>
+      
     </v-container>
 
   </div>
@@ -23,5 +32,23 @@
 
     components: {
     },
+
+    data(){
+      return{
+        projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design a new website again', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+      }
+    },
+
+    computed:{
+      myProjects(){
+        return this.projects.filter((project) => { return project.person === "The Net Ninja"})
+      }
+    }
   }
 </script>
